7b271d00c13a3d838838edd185096cd5ca5dbaf1
Travis CI <travis@travis-ci.org>
2018-01-21 00:02:56 +0800

    test

--- .travis/update.sh
+++ .travis/update.sh
@@ -8,6 +8,7 @@ setup_git() {
 update_file() {
     git remote set-url origin https://${GITHUB_TOKEN}@github.com/hacking-thursday/yuan-data.git > /dev/null 2>&1
     git push --quiet --set-upstream origin master
+    git log -n 3
 }
 
 git status | grep autogen/data.json
