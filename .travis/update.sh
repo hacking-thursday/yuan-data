727128e91fd72baf68ed14f77adf1cd6268c5576
Travis CI <travis@travis-ci.org>
2018-01-20 23:35:58 +0800

    Test GitHub token

---
+++ .travis/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+update_file() {
+    git remote add origin-pages https://${GITHUB_TOKEN}@github.com/hacking-thursday/yuan-data.git > /dev/null 2>&1
+    git push --quiet --set-upstream origin master
+}
+
+RET=$?
+
+if [[ $RET == 0 ]]; then
+    git add autogen/data.json
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    setup_git
+    update_file
+fi
